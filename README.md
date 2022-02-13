9a1b6d45547ca28c7fcec7618de8a8140bdb2a6a
MartienneF <ksana.martyniuk@gmail.com>
2022-02-13 18:58:52 +0200

    Слайдер с исп. swiperJS

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Gloacademy_Batman - Интенсив по верстке "Промосайт фильма «Бэтмен» 2022"
+
+https://github.com/MartienneF/Gloacademy_Batman
+https://MartienneF.github.io/Gloacademy_Batman
+
+Использованы:
+https://swiperjs.com/ - плагин слайдера на JavaScript
--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert('Hello world!');
+const swiper = new Swiper('.main-slider', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: '.main-slider_arrow',
+  },
+  breakpoints: {
+    375: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+    }
+}
+});
